be6f4a8d8828610a0897d1253a4ad81b80733881
Yurii Cherniavskyi <ych@webtrit.com>
2022-12-06 14:10:59 +0200

    Create initial dart package project
    Project created with following command:
    $ dart create -t package gravatar_utils

---
+++ example/gravatar_utils_example.dart
@@ -0,0 +1,6 @@
+import 'package:gravatar_utils/gravatar_utils.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/gravatar_utils.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library gravatar_utils;
+
+export 'src/gravatar_utils_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
